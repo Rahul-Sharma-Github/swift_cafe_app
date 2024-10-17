4806ad13b8d1babd7090c17252b97f11934d7198
Rahul Sharma <rahulsharma3258work@gmail.com>
2024-10-17 11:48:57 +0530

    updated Login Screen Glassmorphic effect | updated text styles everywhere | Added Scrolling to the Column to fix Overflow Problems | Added Gradient color to the Login button |

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -31,142 +33,187 @@ class SwiftCafeLoginScreen extends StatelessWidget {
         ),
         // Glassmorphism effect
         Center(
-          child: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 30),
-            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.2),
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(color: Colors.white.withOpacity(0.2)),
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                // Logo and title
-                Column(
-                  children: [
-                    Container(
-                      width: 89,
-                      height: 70,
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage('assets/cafe_logo.jpg'),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10), // Blur effect
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+                decoration: BoxDecoration(
+                  color:
+                      const Color.fromARGB(255, 248, 243, 243).withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Colors.white.withOpacity(0.2)),
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // Logo and title
+                      Column(
+                        children: [
+                          Container(
+                            width: 89,
+                            height: 70,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage('assets/cafe_logo.jpg'),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Text(
+                            'Swift',
+                            style: GoogleFonts.raleway(
+                              height: 1,
+                              color: Colors.white,
+                              fontSize: 50,
+                              fontWeight: FontWeight.w400,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            'Café',
+                            style: GoogleFonts.raleway(
+                              color: Colors.white,
+                              fontSize: 35,
+                              fontWeight: FontWeight.w400,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          const Text(
+                            '“Latte but never late”',
+                            style: TextStyle(
+                              height: 3,
+                              fontSize: 14,
+                              color: Colors.white60,
+                              fontWeight: FontWeight.w400,
+                              shadows: [
+                                Shadow(
+                                  color: Colors.white,
+                                  blurRadius: 30.0,
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 30),
+                      // Input fields
+                      TextField(
+                        style: const TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          labelText: 'User Name',
+                          labelStyle: const TextStyle(
+                              color: Colors.white, fontSize: 14),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.white.withOpacity(0.7)),
+                          ),
+                          focusedBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 20),
-                    Text(
-                      'Swift',
-                      style: GoogleFonts.raleway(
-                        color: Colors.white,
-                        fontSize: 40,
-                        fontWeight: FontWeight.w600,
+                      const SizedBox(height: 20),
+                      TextField(
+                        obscureText: true,
+                        style: const TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: const TextStyle(
+                              color: Colors.white, fontSize: 14),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.white.withOpacity(0.7)),
+                          ),
+                          focusedBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        ),
                       ),
-                      textAlign: TextAlign.center,
-                    ),
-                    Text(
-                      'Café',
-                      style: GoogleFonts.raleway(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.w400,
+                      const SizedBox(height: 30),
+                      // Login button
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets
+                              .zero, // To remove default padding for the Ink widget
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(33),
+                          ),
+                        ),
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            // ignore: prefer_const_constructors
+                            gradient: LinearGradient(
+                              colors: const [
+                                Color.fromARGB(255, 62, 39, 35),
+                                Color.fromARGB(255, 167, 123, 107)
+                              ],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                            ),
+                            borderRadius: BorderRadius.circular(33),
+                          ),
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(vertical: 14),
+                            constraints:
+                                const BoxConstraints(minWidth: double.infinity),
+                            child: const Center(
+                              child: Text(
+                                'Login',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
-                      textAlign: TextAlign.center,
-                    ),
-                    Text(
-                      '“Latte but never late”',
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.white.withOpacity(0.7),
+                      const SizedBox(height: 15),
+                      // Signup button
+                      OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(vertical: 14),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(33),
+                          ),
+                          side: const BorderSide(color: Colors.white),
+                        ),
+                        child: const SizedBox(
+                          width: double.infinity,
+                          child: Center(
+                            child: Text(
+                              'Signup',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 30),
-                // Input fields
-                TextField(
-                  style: const TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    labelText: 'User Name',
-                    labelStyle: const TextStyle(color: Colors.white),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Colors.white.withOpacity(0.7)),
-                    ),
-                    focusedBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                TextField(
-                  obscureText: true,
-                  style: const TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    labelStyle: const TextStyle(color: Colors.white),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Colors.white.withOpacity(0.7)),
-                    ),
-                    focusedBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 30),
-                // Login button
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(vertical: 14),
-                    backgroundColor: Colors.brown[400],
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  child: const SizedBox(
-                    width: double.infinity,
-                    child: Center(
-                      child: Text(
-                        'Login',
-                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      const SizedBox(height: 20),
+                      // Privacy policy
+                      Text(
+                        'Privacy Policy',
+                        style: TextStyle(
+                          height: 2,
+                          fontSize: 16,
+                          color: Colors.white.withOpacity(0.8),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 15),
-                // Signup button
-                OutlinedButton(
-                  onPressed: () {},
-                  style: OutlinedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(vertical: 14),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    side: const BorderSide(color: Colors.white),
-                  ),
-                  child: const SizedBox(
-                    width: double.infinity,
-                    child: Center(
-                      child: Text(
-                        'Signup',
-                        style: TextStyle(fontSize: 18, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                // Privacy policy
-                Text(
-                  'Privacy Policy',
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.white.withOpacity(0.8),
-                    decoration: TextDecoration.underline,
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         ),
